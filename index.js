01cedfe90c0a2328b99d9a36e05d024292b2b1fb
Denisha Garner <thegarnercode@gmail.com>
2022-04-20 18:43:33 -0400

    fetch request

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 console.log("Hello, World");
 
-document.addEventListener('DOMContentLoaded'),()=>{
+document.addEventListener('DOMContentLoaded', () => {
     fetch ('https://api.coinpaprika.com/v1/coins')
     .then(res => res.json())
-    .then(arrCoinObj => renderAllCoins(arrCoinObj))
+    .then(arrCoinObj => console.log(arrCoinObj))//renderAllCoins(arrCoinObj))
     
-}
+});
 
 function renderAllCoins(arrCoinObj) {
     arrCoinObj.forEach(coinObj => renderOneCoin(coinObj))
+}
+
+function renderOneCoin(coinObj){
+
 }
