57ae6056a7778e3500d62f51b51264472a3b281c
Denisha Garner <thegarnercode@gmail.com>
2022-04-20 17:20:14 -0400

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log("Hello, World");
+
+document.addEventListener('DOMContentLoaded'),()=>{
+    fetch ('https://api.coinpaprika.com/v1/coins')
+    .then(res => res.json())
+    .then(arrCoinObj => renderAllCoins(arrCoinObj))
+    
+}
+
+function renderAllCoins(arrCoinObj) {
+    arrCoinObj.forEach(coinObj => renderOneCoin(coinObj))
+}
