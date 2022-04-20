78c3a230fbdf451ae899f5bfb3a8d86ae4447f09
Mays Jones <maysjones2@gmail.com>
2022-04-20 17:50:57 -0400

    test

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 console.log("Hello, World");
+const Url = "https://api.coinpaprika.com/v1/coins"
+const dataTable = document.getElementById("data")
+
+
+fetch(Url)
+.then (res => res.json())
+.then (data => displayData(data))
+
+function displayData(data){
+    console.log(data)
+    const topTen = data.slice(0,10)
+    console.log(topTen)
+    const newRow = document.createElement("tr")
+    console.log(data.forEach(data =>{
+        dataTable.appendChild(newRow)
+    }))}
