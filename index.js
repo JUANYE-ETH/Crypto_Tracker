ab2b53123ecc9b3f293e92425e7e1d99326837e0
Juan Velasco <juanayevela@yahoo.com>
2022-04-21 19:27:29 -0400

    added coins to the table rows

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log("Hello, World");
+//DOMContentLoaded Event
+document.addEventListener("DOMContentLoaded", getAllCoins)
+
+// console.log("Hello, World");
+const Url = "https://api.coingecko.com/api/v3/exchange_rates"
+const dataTable = document.getElementById("data")
+
+
+function getAllCoins() {
+    fetch(Url)
+        .then((res) => res.json())
+        .then((data) => displayData(data.rates))
+}
+
+
+function displayData(coinsObj) {
+    const coinsArr = [];
+
+    for (const key in coinsObj) {
+        coinsArr.push(coinsObj[key])
+    }
+
+    
+    coinsArr.slice(0, 12).forEach(coin => {
+        const name = coin.name;
+        const newRow = document.createElement("tr");
+        
+        const newCellName = document.createElement("td");
+        newCellName.textContent = coin.name;
+
+        const newCellType = document.createElement("td");
+        newCellType.textContent = coin.type;
+
+        const newCellUnit = document.createElement("td");
+        newCellUnit.textContent = coin.unit;
+
+        const newCellValue = document.createElement("td");
+        newCellValue.textContent = Math.round(coin.value);
+        
+        newRow.append(newCellName, newCellType, newCellUnit, newCellValue);
+        document.getElementById('data').append(newRow)
+    });
+}
